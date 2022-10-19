59d2a3b2f0754e78d2b0cdddcb6ade4463800cd7
Le Pham <phamtle724@gmail.com>
2022-10-18 20:13:03 -0700

    redeploy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Homepage from "./Homepage";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
